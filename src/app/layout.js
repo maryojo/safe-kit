878ce22c69527dc4a0afc03272fd0bfd2f15b927
Mary Ojo <38431519+maryojo@users.noreply.github.com>
2024-10-19 11:51:27 +0100

    Add PWA functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,11 @@ import AuthProvider from '@/components/AuthProvider'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <meta name="theme-color" content="#fff" />
+      </head>
       <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
